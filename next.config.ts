40c93ee13e6fb80761f78e941cf7f287b7255133
NIJAT <abushov@proton.me>
2025-12-29 18:28:59 +0400

    fix: set static export unconditionally for CF Pages build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: (process.env.STATIC_EXPORT || process.env.CF_PAGES) ? 'export' : 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
